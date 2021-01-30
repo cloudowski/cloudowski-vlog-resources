6e3dcc9cd78e488de93d7f65bcfbdcdba1e44e98
Tomasz Cholewa <tomasz@cloudowski.com>
2021-01-30 16:07:50 +0100

    wip

--- 2021-1-30-skaffold/flask-helloworld/src/app.py
+++ 2021-1-30-skaffold/flask-helloworld/src/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     h = socket.gethostname()
-    return h + ': v0.0.5 Hello, World!!!\n'
+    return h + ': Hello, World!!!\n'
 
 
 app.run(host='0.0.0.0', port=8080)
